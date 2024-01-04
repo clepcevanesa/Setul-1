b0552340fc90c954d6e9cb5cd8add81ca38619a6
clepcevanesa <155635810+clepcevanesa@users.noreply.github.com>
2024-01-04 16:18:56 +0200

    Add files via upload

---
+++ App13/Program.cs
@@ -0,0 +1,32 @@
+﻿//Determianti cati ani bisecti sunt intre anii y1 si y2.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduceti primul an: ");
+        int y1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Introduceti al doilea an: ");
+        int y2 = int.Parse(Console.ReadLine());
+
+        int count = CountLeapYears(y1, y2);
+
+        Console.WriteLine($"intre anii {y1} și {y2} sunt {count} ani bisecti.");
+    }
+
+    static int CountLeapYears(int year1, int year2)
+    {
+        int count = 0;
+        for (int year = year1; year <= year2; year++)
+        {
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
