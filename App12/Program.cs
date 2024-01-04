fde55562664b71c2f32f1fd98bb8b80df717eeab
clepcevanesa <155635810+clepcevanesa@users.noreply.github.com>
2024-01-04 16:18:16 +0200

    Add files via upload

---
+++ App12/Program.cs
@@ -0,0 +1,30 @@
+﻿//Determinati cate numere integi divizibile cu n se afla in intervalul [a, b]. 
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.Write("Introduceti n: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Introduceti a: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Introduceti b: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        int count = 0;
+        for (int i = a; i <= b; i++)
+        {
+            if (i % n == 0)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine($"Numere divizibile cu {n} în intervalul [{a}, {b}]: {count}");
+    }
+}
