54e584c0efe1f060e82eedd4e5886748245fb581
clepcevanesa <155635810+clepcevanesa@users.noreply.github.com>
2024-01-04 16:15:56 +0200

    Add files via upload

---
+++ App9/Program.cs
@@ -0,0 +1,21 @@
+﻿//Afisati toti divizorii numarului n. 
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduceti un numar intreg pozitiv: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Divizorii numarului {n} sunt:");
+        for (int i = 1; i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
