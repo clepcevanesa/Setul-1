41ad0c311e1941cfc9ba4d7e360228ab84175dd0
clepcevanesa <155635810+clepcevanesa@users.noreply.github.com>
2024-01-04 16:11:48 +0200

    Add files via upload

---
+++ App4/Program.cs
@@ -0,0 +1,20 @@
+﻿//Detreminati daca un an y este an bisect.
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Introduceti anul:");
+        int an=Convert.ToInt32(Console.ReadLine());
+
+        if ((an%4==0 && an%100!=0) || (an%400==0))
+        {
+            Console.WriteLine($"{an} este un an bisect");
+        }
+        else
+        {
+            Console.WriteLine($"{an} nu este un an bisect");
+        }
+    }
+}
