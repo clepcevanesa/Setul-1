fb1d3f7d39728bc27b57850c901306e3d2020e69
clepcevanesa <155635810+clepcevanesa@users.noreply.github.com>
2024-01-04 16:15:13 +0200

    Add files via upload

---
+++ App8/Program.cs
@@ -0,0 +1,23 @@
+﻿//(Swap restrictionat) Se dau doua variabile numerice a si b ale carori valori sunt date de intrare. Se cere sa se inverseze valorile lor fara a folosi alte variabile suplimentare.  
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Introduceti valoarea pentru a:");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Introduceti valoarea pentru b:");
+        int b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Inainte de inversare: a = {a}, b = {b}");
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        Console.WriteLine($"Dupa inversare: a = {a}, b = {b}");
+    }
+}
