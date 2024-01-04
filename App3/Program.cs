77df75327c07bca6617332cb7201260e61be69df
clepcevanesa <155635810+clepcevanesa@users.noreply.github.com>
2024-01-04 16:11:05 +0200

    Add files via upload

---
+++ App3/Program.cs
@@ -0,0 +1,23 @@
+﻿//Determinati daca n se divide cu k, unde n si k sunt date de intrare. 
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Introduceti n:");
+        int n=Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Introduceti k:");
+        int k=Convert.ToInt32(Console.ReadLine());
+
+        if (n%k==0)
+        {
+            Console.WriteLine($"{n} se divide cu {k}");
+        }
+        else
+        {
+            Console.WriteLine($"{n} nu se divide cu {k}");
+        }
+     }    
+}
