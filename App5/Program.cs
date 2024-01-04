5c9454a4f0e21721d22f9b0273b8af81fa3002fd
clepcevanesa <155635810+clepcevanesa@users.noreply.github.com>
2024-01-04 16:13:43 +0200

    Add files via upload

---
+++ App5/Program.cs
@@ -0,0 +1,27 @@
+﻿//Extrageti si afisati a k-a cifra de la sfarsitul unui numar. Cifrele se numara de la dreapta la stanga. 
+
+using System;
+
+class MainClass
+{
+    static void Main()
+    {
+        Console.WriteLine("Introduceti numarul:");
+        int numar = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Introduceti valoarea lui k:");
+        int k = int.Parse(Console.ReadLine());
+
+        int cifra = ExtrageCifra(numar, k);
+        Console.WriteLine($"Cifra a {k}-a de la sfarsitul numarului este: {cifra}");
+    }
+
+    static int ExtrageCifra(int numar, int k)
+    {
+        for (int i = 1; i < k; i++)
+        {
+            numar /= 10;
+        }
+        return numar % 10;
+    }
+}
