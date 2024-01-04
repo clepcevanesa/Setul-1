2fbfe418d99f9c18513443222dad532ec2aa1f21
clepcevanesa <155635810+clepcevanesa@users.noreply.github.com>
2024-01-04 16:16:36 +0200

    Add files via upload

---
+++ App10/Program.cs
@@ -0,0 +1,41 @@
+﻿//Test de primalitate: determinati daca un numar n este prim.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduceti un numar: ");
+        int numar = Convert.ToInt32(Console.ReadLine());
+
+        if (EstePrim(numar))
+        {
+            Console.WriteLine($"{numar} este un numar prim.");
+        }
+        else
+        {
+            Console.WriteLine($"{numar} nu este un numar prim.");
+        }
+
+        Console.ReadLine();
+    }
+
+    static bool EstePrim(int numar)
+    {
+        if (numar <= 1)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.Sqrt(numar); i++)
+        {
+            if (numar % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
