2a1ca9b779ce79f1bc26ad6831f72537a59f68f0
clepcevanesa <155635810+clepcevanesa@users.noreply.github.com>
2024-01-04 16:17:25 +0200

    Add files via upload

---
+++ App11/Program.cs
@@ -0,0 +1,22 @@
+﻿//Afisati in ordine inversa cifrele unui numar n. 
+
+using System;
+
+class ReverseNumber
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Introduceti un numar: ");
+        int numar = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Cifrele numarului in ordine inversa sunt: ");
+        while (numar > 0)
+        {
+            int cifra = numar % 10;
+            Console.Write(cifra);
+            numar = numar / 10;
+        }
+
+        Console.WriteLine();
+    }
+}
